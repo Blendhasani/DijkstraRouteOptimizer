8aaf18ca4ece4773857bd6e2459864077d7e6964
Blendhasani <bh51257@ubt-uni.net>
2025-04-03 08:13:35 +0200

    Created Dijkstra class and added method structure for shortest path calculation

---
+++ Dijkstra.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DijkstraRouteOptimizer
+{
+	public static class Dijkstra
+	{
+		public static Dictionary<int, int> FindShortestPaths(
+			Dictionary<int, List<(int neighbor, int weight)>> graph,
+			int source)
+		{
+			// TODO: Implementimi i Algoritmit Dijkstra
+			return new Dictionary<int, int>();
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DijkstraRouteOptimizer;
+
 var graph = new Dictionary<int, List<(int, int)>>()
 {
 	{ 0, new List<(int, int)> { (1, 4), (2, 1) } },
@@ -7,14 +9,11 @@ var graph = new Dictionary<int, List<(int, int)>>()
 	{ 3, new List<(int, int)>() }
 };
 
-// Print the graph
-foreach (var node in graph)
-{
-	Console.Write($"Node {node.Key} -> ");
-	foreach (var (neighbor, weight) in node.Value)
-		Console.Write($"({neighbor}, {weight}) ");
-	Console.WriteLine();
-}
 
+var distances = Dijkstra.FindShortestPaths(graph, 0);
+
+// Printimi i rezultatit per te treguar se struktura funksionon
+foreach (var kvp in distances)
+	Console.WriteLine($"Shortest distance from 0 to {kvp.Key} is {kvp.Value}");
 Console.ReadLine();
 
